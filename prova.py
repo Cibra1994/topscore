6a4d9939fbf25ccf5729dfa045801e7360e978f7
Cibra1994 <71648658+Cibra1994@users.noreply.github.com>
2020-10-05 17:37:10 +0200

    Update prova.py

--- prova.py
+++ prova.py
@@ -10,50 +10,6 @@ class Main():
     def __init__(self):
 
         self.s = requests.Session()
-
-        self.url = 'http://www.topscore90.net/Holder.bet?page=sport'
-        self.quote = "http://www.topscore90.net/res/Client/wbe/proxybetting.aspx?method=WIDGET_LIST&type=json&st=mm&wts=BS,NE&ts=1600696795291"
-
-        self.get_page()
-
-
-
-    def get_page(self):
-
-        try:
-
-            print("Getting product page...")
-
-            headers = {
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'Accept-Encoding': 'gzip, deflate',
-                'Accept-Language': 'it',
-                'Connection': 'keep-alive',
-                'Host': 'www.topscore90.net',
-                'Upgrade-Insecure-Requests': '1',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
-            }
-
-            r = self.s.get(self.url, headers = headers)
-
-            if r.status_code == 200:
-
-                print("Got product page")
-                self.caricoquote()
-
-            else:
-                print(f"Error {r.status_code}")import requests
-import json
-from bs4 import BeautifulSoup
-import time
-
-
-
-class Main():
-
-    def __init__(self):
-
-        self.s = requests.Session()
         self.url = 'http://www.topscore90.net/Holder.bet?page=sport'
         self.quote_topscore= "http://www.topscore90.net/res/Client/wbe/proxybetting.aspx?method=WIDGET_LIST&type=json&st=mm&wts=BS,NE&ts=1600696795291"
         self.quote_betn1='https://oddsfeed2.betn1.com/oddsfeed/lite/loadData/highlightDataJSON.jsp?langtag=it-IT-bn1'
@@ -107,7 +63,6 @@ class Main():
                 'Accept-Encoding': 'gzip, deflate',
                 'Accept-Language': 'it',
                 'Connection': 'keep-alive',
-                
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
                 'X-Requested-With': 'XMLHttpRequest'
             }
@@ -118,7 +73,7 @@ class Main():
                 
                 self.formato_json_betn1=json.loads(r.text)
             
-                print(json.dumps(self.formato_json_betn1, indent=4, sort_keys=True))
+                print(json.dumps(self.formato_json_betn1, indent=4))
 
             else:
                 print(f"Error {r.status_code}")
@@ -126,29 +81,49 @@ class Main():
 
         except Exception as e :
             print(e)
-
+   
     def parser_betn1(self):
-        self.nome_partita_betn1=[]
-        self.nome_betn1=[]
-        self.quote_betn1=[]
-        self.quote_uno_betn1=[]
+      
         try:
+            self.uno=[]
+            self.x=[]
+            self.due=[]
+            self.nome=[]
+            self.codice=[]
+            self.quote=[]
             
-            
-            for self.eventi_betn1 in range(len(self.formato_json_betn1)):
-                
-                if self.formato_json_betn1[self.eventi_betn1]['_bsolType']=='avv':
+            for element in self.formato_json_betn1:
+                if 'avvenimenti'in element:
+                    h=element['avvenimenti']
+                    for elementiii in range(len(h)):
+                        l=h[elementiii]['desc']
+                        self.nome.append(l)
+                        
                     
-                    self.nome_partita_betn1.append(self.formato_json_betn1[self.eventi_betn1]['avvenimenti'][0]['desc'])
-            for self.quote in range(len(self.formato_json_betn1)):
-                  if self.formato_json_betn1[self.quote]['_bsolType']=='scomm':
-                       self.quote_betn1.append(self.formato_json_betn1[self.quote]['scommesse'][0]['quoteOrdinate'][0])
-                       float i = self.quote_betn1[0]
-                       
-                       self.quote_uno_betn1.append(self.formato_json_betn1[self.quote]['scommesse'][0]['quote'][i])
-                                           
-  
-
+                   
+                if 'scommesse' in element:
+                    a=element['scommesse']
+                    for elementi in a:
+                        if 'quote'in elementi:
+                            b=elementi['quote']
+                            c=elementi['quoteOrdinate']
+                            for elementii in range(len(c)):
+                                f='{}'.format(c[elementii])
+                                if b[f]["descE"]=='1':
+                                    d=b[f]['vt']
+                                    self.uno.append(d)
+                                if b[f]["descE"]=='X':
+                                    d=b[f]['vt']
+                                    self.x.append(d)
+                                if b[f]["descE"]=='2':
+                                    d=b[f]['vt']
+                                    self.due.append(d)
+                quote=zip(self.uno,self.x,self.due)
+                quote_list=list(quote)
+                partita=zip(self.nome,quote_list)
+                self.partita_list=list(partita)
+ 
+                
         except Exception as e :
             
             print(e)
@@ -157,37 +132,10 @@ class Main():
 
    
     def parser_topscore(self):
-        self.nome_evento=[]
-        self.dettagli_evento=[]
-        self.dettagli_singolo_evento=[]
-        self.quote_uno=[]
-        self.quote_due=[]
-        self.quote_X=[]
+        self.quote_topscore=[]
+      
 
-        try:
-            
-            
-            for self.eventi in range(len(self.formato_json_topscore['ListData_BS'])):
-                self.nome_evento.append(self.formato_json_topscore['ListData_BS'][self.eventi]['EventName'])
-                self.dettagli_evento.append(self.formato_json_topscore['ListData_BS'][self.eventi]['GameDetails'])
-                self.elementi=0;
-                for self.elementi in range(len( self.dettagli_evento[self.eventi])):
-                    if self.dettagli_evento[self.eventi][self.elementi]['GameAlias']=='Finale 1X2':
-                        if self.dettagli_evento[self.eventi][self.elementi]['GameName'] == '1':
-                            self.quote_uno.append(self.dettagli_evento[self.eventi][self.elementi]['GameOdd'])
-                            
-                        if self.dettagli_evento[self.eventi][self.elementi]['GameName'] == 'X':
-                            self.quote_X.append(self.dettagli_evento[self.eventi][self.elementi]['GameOdd'])
-                        if self.dettagli_evento[self.eventi][self.elementi]['GameName'] == '2':
-                            self.quote_due.append(self.dettagli_evento[self.eventi][self.elementi]['GameOdd'])        
-                    
-                                    
-                
-
-        except Exception as e :
-            
-            print(e)
-            
+     
 
 
 
@@ -198,6 +146,7 @@ class Main():
 
             
     def stampa_topscore(self):
+        
          
         try:
               for x in range(len(self.formato_json_topscore['ListData_BS'])):
@@ -211,8 +160,11 @@ class Main():
     def stampa_betn1(self):
          
         try:
-            print(json.dumps(self.quote_betn1,indent=3))
-            print(json.dumps(self.quote_uno_betn1,indent=3))
+            
+                print(self.partita_list)
+                
+          
+            
         except Exception as e :
                     
                         print(e)
@@ -220,70 +172,5 @@ class Main():
 
 
    
-if __name__ == "__main__":
-    Main()
-
-                time.sleep(10)
-
-        except Exception as e :
-            print(e)
-
-
-    def caricoquote(self):
-
-        try:
-
-            headers = {
-                'Accept': 'application/json, text/javascript, */*; q=0.01',
-                'Accept-Encoding': 'gzip, deflate',
-                'Accept-Language': 'it',
-                'Connection': 'keep-alive',
-                'Host': 'www.topscore90.net',
-                'Origin': 'http://www.topscore90.net',
-                'Referer': 'http://www.topscore90.net/Holder.bet?page=sport',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
-                'X-Requested-With': 'XMLHttpRequest'
-            }
-
-            r = self.s.post(self.quote, headers = headers)
-
-            if r.status_code == 200:
-
-                print(r.text)
-                self.covertitore_json()
-            else:
-                print(f"Error {r.status_code}")
-                time.sleep(10)
-
-        except Exception as e :
-            print(e)
-
-
-
-    def covertitore_json(self):
-            
-        try:
-            
-            headers = {
-                'Accept': 'application/json, text/javascript, */*; q=0.01',
-                'Accept-Encoding': 'gzip, deflate',
-                'Accept-Language': 'it',
-                'Connection': 'keep-alive',
-                'Host': 'www.topscore90.net',
-                'Origin': 'http://www.topscore90.net',
-                'Referer': 'http://www.topscore90.net/Holder.bet?page=sport',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
-                'X-Requested-With': 'XMLHttpRequest'
-            }
-            r = self.s.post(self.quote, headers = headers)
-                
-            formato_json=json.loads(r.text)
-                
-            print(json.dumps(formato_json, indent=4, sort_keys=True))
-
-        except Exception as e :
-                 print(e)
-
-
 if __name__ == "__main__":
     Main()
